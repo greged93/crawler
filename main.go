5c3b5a4c422acb70ec98d0617a4c0559ae24a051
Gregory Edison <gregory.edison1993@gmail.com>
2022-09-21 09:51:06 +0200

    chore: cleaning

--- main.go
+++ main.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
-	"os"
 )
 
 var (
 	fileName = ""
+	parse    = true
 )
 
 func main() {
 	flag.StringVar(&fileName, "filename", "", "ingredient json file")
+	flag.BoolVar(&parse, "parse", true, "parse results if true, otherwise crawl")
 	flag.Parse()
 	if fileName == "" {
 		log.Fatal("missing filename")
 		return
 	}
-	//Crawl(fileName)
-	var ingredients = make(map[string]int)
-	f, err := os.Open(fileName)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-		return
+	if parse {
+		Parse()
+	} else {
+		Crawl(fileName)
 	}
-	dec := json.NewDecoder(f)
-	err = dec.Decode(&ingredients)
-	if err != nil {
-		log.Fatalf("error decoding file: %v", err)
-		return
-	}
-	fmt.Printf("found %d ingredients \n", len(ingredients))
-	var frequentlyUsed []string
-	for ing, rep := range ingredients {
-		if rep > 3 {
-			frequentlyUsed = append(frequentlyUsed, ing)
-		}
-	}
-	fmt.Println("got", len(frequentlyUsed), "ingredients")
 }
