baa8848e673f2d5f7e1518d3757329414dda02d0
Gregory Edison <gregory.edison1993@gmail.com>
2022-11-20 12:48:26 +0100

    feat: clean flag

--- main.go
+++ main.go
@@ -7,12 +7,14 @@ import (
 
 var (
 	fileName = ""
-	parse    = true
+	parse    = false
+	clean    = false
 )
 
 func main() {
 	flag.StringVar(&fileName, "filename", "", "ingredient json file")
-	flag.BoolVar(&parse, "parse", true, "parse results if true, otherwise crawl")
+	flag.BoolVar(&parse, "parse", false, "parse results if true, otherwise continue")
+	flag.BoolVar(&clean, "clean", false, "clean results if true, otherwise continue")
 	flag.Parse()
 	if fileName == "" {
 		log.Fatal("missing filename")
@@ -20,6 +22,8 @@ func main() {
 	}
 	if parse {
 		Parse()
+	} else if clean {
+		Clean()
 	} else {
 		Crawl(fileName)
 	}
